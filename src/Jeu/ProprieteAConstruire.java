4578f165c87c750750cf7f5c800fa8b9e692d919
pinetq <quentin.pinet@etu.upmf-grenoble.fr>
2016-05-24 16:08:52 +0200

    Merge origin/master

--- src/Jeu/ProprieteAConstruire.java
+++ src/Jeu/ProprieteAConstruire.java
@@ -5,9 +5,9 @@ import Jeu.Groupe;
 
 public class ProprieteAConstruire extends Propriete {
 
-	private Groupe couleur;
-	private int prix;
-	private int loyer;
+    private Groupe  couleur;
+    private int     prix;
+    private int     loyer;
 
     public ProprieteAConstruire(String nm, int prx) {
         super(nm, prx);
@@ -20,35 +20,29 @@ public class ProprieteAConstruire extends Propriete {
         
     }
 
-        @Override
-	public int getPrix() {
-		return this.prix;
-	}
-
-	public void setPrix(int prix) {
-		this.prix = prix;
-	}
-
-	public int getLoyer() {
-		return this.loyer;
-	}
-
-	public void setLoyer(int loyer) {
-		this.loyer = loyer;
-	}
-
-    /**
-     * @return the couleur
-     */
+    @Override
+    public int getPrix() {
+	return this.prix;
+    }
+    
+    public void setPrix(int prix) {
+	this.prix = prix;
+    }
+    
+    public int getLoyer() {
+        return this.loyer;
+    }
+    
+    public void setLoyer(int loyer) {
+        this.loyer = loyer;
+    }
+    
     public Groupe getCouleur() {
         return couleur;
     }
-
-    /**
-     * @param couleur the couleur to set
-     */
+    
     public void setCouleur(Groupe couleur) {
         this.couleur = couleur;
     }
-
+    
 }
