5a629a3df379139f6fd92aeda7b63875fd9cf082
pinetq <quentin.pinet@etu.upmf-grenoble.fr>
2016-06-07 11:45:01 +0200

    Merge origin/master
    Conflicts:
    	src/Jeu/Monopoly.java

--- src/Jeu/Monopoly.java
+++ src/Jeu/Monopoly.java
@@ -89,22 +89,22 @@ public class Monopoly {
                 String ActionCarteType = data.get(i)[0];
                 if (ActionCarteType.compareTo("SP") == 0) {
                     Carte nouvelleCarte = new Carte(ActionsCarte.SP,data.get(i)[0],0,0);
-                    getCartesChance().add(nouvelleCarte);
+                    cartesChance.add(nouvelleCarte);
                 } else if (ActionCarteType.compareTo("RE") == 0) {
                     Carte nouvelleCarte = new Carte(ActionsCarte.RE,data.get(i)[0],Integer.parseInt(data.get(i)[2]),0);
-                    getCartesChance().add(nouvelleCarte);
+                    cartesChance.add(nouvelleCarte);
                 } else if (ActionCarteType.compareTo("MH") == 0) {
                     Carte nouvelleCarte = new Carte(ActionsCarte.MH,data.get(i)[0],Integer.parseInt(data.get(i)[2]),Integer.parseInt(data.get(i)[3]));
-                    getCartesChance().add(nouvelleCarte);
+                    cartesChance.add(nouvelleCarte);
                 }else if (ActionCarteType.compareTo("GP") == 0) {
                     Carte nouvelleCarte = new Carte(ActionsCarte.GP,data.get(i)[0],Integer.parseInt(data.get(i)[2]),0);
-                    getCartesChance().add(nouvelleCarte);
+                    cartesChance.add(nouvelleCarte);
                 }else if (ActionCarteType.compareTo("AV") == 0) {
                     Carte nouvelleCarte = new Carte(ActionsCarte.AV,data.get(i)[0],Integer.parseInt(data.get(i)[2]),0);
-                    getCartesChance().add(nouvelleCarte);
+                    cartesChance.add(nouvelleCarte);
                 }else if (ActionCarteType.compareTo("AP") == 0) {
                     Carte nouvelleCarte = new Carte(ActionsCarte.AP,data.get(i)[0],Integer.parseInt(data.get(i)[2]),0);
-                    getCartesChance().add(nouvelleCarte);
+                    cartesChance.add(nouvelleCarte);
                 }else {
                     System.err.println("[buildGamePlateau()] : unknown action at "+i);
                 }
@@ -126,24 +126,24 @@ public class Monopoly {
                 String ActionCarteType = data.get(i)[0];
                 if (ActionCarteType.compareTo("SP") == 0) {
                     Carte nouvelleCarte = new Carte(ActionsCarte.SP,data.get(i)[0],0,0);
-                    getCartesChance().add(nouvelleCarte);
+                    cartesChance.add(nouvelleCarte);
                 }
                 else if (ActionCarteType.compareTo("GP") == 0) {
                     Carte nouvelleCarte = new Carte(ActionsCarte.GP,data.get(i)[0],Integer.parseInt(data.get(i)[2]),0);
-                    getCartesChance().add(nouvelleCarte);
+                    cartesChance.add(nouvelleCarte);
                 }
                 else if (ActionCarteType.compareTo("AN") == 0) {
                     Carte nouvelleCarte = new Carte(ActionsCarte.AN,data.get(i)[0],Integer.parseInt(data.get(i)[2]),0);
-                    getCartesChance().add(nouvelleCarte);
+                    cartesChance.add(nouvelleCarte);
                 }else if (ActionCarteType.compareTo("DE") == 0) {
                     Carte nouvelleCarte = new Carte(ActionsCarte.DE,data.get(i)[0],Integer.parseInt(data.get(i)[2]),0);
-                    getCartesChance().add(nouvelleCarte);
+                    cartesChance.add(nouvelleCarte);
                 }else if (ActionCarteType.compareTo("AP") == 0) {
                     Carte nouvelleCarte = new Carte(ActionsCarte.AP,data.get(i)[0],Integer.parseInt(data.get(i)[2]),0);
-                    getCartesChance().add(nouvelleCarte);
+                    cartesChance.add(nouvelleCarte);
                 }else if (ActionCarteType.compareTo("AV") == 0) {
                     Carte nouvelleCarte = new Carte(ActionsCarte.AV,data.get(i)[0],Integer.parseInt(data.get(i)[2]),0);
-                    getCartesChance().add(nouvelleCarte);
+                    cartesChance.add(nouvelleCarte);
                 }else {
                     System.err.println("[buildGamePlateau()] : unknown action at "+i);
                 }
