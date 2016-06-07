a10b8c6d8c8bab01d0a0e667e9f9753b0a7a903b
pinetq <quentin.pinet@etu.upmf-grenoble.fr>
2016-06-07 10:49:22 +0200

    Merge origin/master
    Conflicts:
    	src/Jeu/Monopoly.java

--- src/Jeu/Carte.java
+++ src/Jeu/Carte.java
@@ -3,11 +3,13 @@ package Jeu;
 public class Carte {
     
     private int numero;
+    private String texte;
     private Data.ActionsCarte action;
     private int x = 0;
     private int y = 0;
     
-    public Carte(Data.ActionsCarte action, int numero, int x, int y) {
+    public Carte(int numero, String texte, Data.ActionsCarte action, int x, int y) {
+        this.texte = texte;
         this.numero = numero;
         this.action = action;
         this.x = x;
