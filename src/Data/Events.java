5f718f89f41925d841e82fc791815525b5121ff1
coulonv <coulonv@pc-dg-025-04>
2016-06-08 09:38:43 +0200

    Merge origin/master into HEAD

--- src/Data/Events.java
+++ src/Data/Events.java
@@ -36,10 +36,39 @@ public class Events {
         acte = action;
         jjoue = j1;
         jreceveur = j2;
-        montant = montant;
+        montant = prix;
     }
     
     public String message(){
-        return "pas encore fait";
+        String message = "";
+        switch(acte.toString()){
+            case "gain":
+                message = jjoue.getNomJoueur() + " a gagné " + montant + "€";
+                break;
+            case "payerLoyer":
+                message = jjoue.getNomJoueur() + " est arrivé sur la propriete de " + jreceveur.getNomJoueur() + " et lui a versé" + montant + "€";
+                break;
+            case "acheter":
+                message = jjoue + " a acheté la propriete " + jjoue.getPositionCourante().getNom() + " pour " + montant + "€";
+                break;
+            case "payer":
+                message = jjoue + " paye " + montant + "€";
+                break;
+            case "neRienFaire":
+                message = "Il ne se passe rien...";
+                break;
+            case "carteChance":
+                message = jjoue + " tire une carte Chance";
+                break;
+            case "carteCommunaute":
+                message = jjoue + " tire une carte Caisse de Communauté";
+                break;
+            case "prison":
+                message = jjoue + " va tout droit à la prison. Eat shit m*therf*cking nigga";
+                break;
+            default:
+                message = "ce cas n'est pas géré";
+        }
+        return message;
+        }
     }
-}
