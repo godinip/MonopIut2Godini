66d4eefb41ecc0c0fd084d2e45396a3f633a3bd8
coulonv <coulonv@pc-dg-025-04>
2016-06-08 10:42:17 +0200

    Merge origin/master into HEAD

--- src/Jeu/Communaute.java
+++ src/Jeu/Communaute.java
@@ -1,9 +1,11 @@
 package Jeu;
 
+import Data.*;
+
 public class Communaute extends Carreau {
 
     public Communaute(String nom, int numero) {
-        super(nom, numero);
+        super(numero, nom);
     }
     
     public Events action(Joueur J){
