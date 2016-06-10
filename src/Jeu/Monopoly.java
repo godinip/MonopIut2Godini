aa6e32cd685d8c3908a322504ba97767984a43f1
coulonv <coulonv@pc-dg-025-04>
2016-06-10 15:22:34 +0200

    Merge origin/master into HEAD

--- src/Jeu/Monopoly.java
+++ src/Jeu/Monopoly.java
@@ -43,7 +43,7 @@ public class Monopoly {
                             Groupe nouvGroupe = new Groupe(CouleurPropriete.valueOf(data.get(i)[3]));
                             getGroupes().put(data.get(i)[3], nouvGroupe);
                         }
-                        ProprieteAConstruire nouvellePropriete = new ProprieteAConstruire(Integer.parseInt(data.get(i)[1]), data.get(i)[2], Integer.parseInt(data.get(i)[4]),getGroupes().get(data.get(i)[3]),loyer);
+                        ProprieteAConstruire nouvellePropriete = new ProprieteAConstruire(Integer.parseInt(data.get(i)[1]), data.get(i)[2], Integer.parseInt(data.get(i)[4]),getGroupes().get(data.get(i)[3]),loyer,Integer.parseInt(data.get(i)[11]));
                         getCarreaux().put(nouvellePropriete.getNumero(), nouvellePropriete);
                     }
                     else if (caseType.compareTo("G") == 0) {
